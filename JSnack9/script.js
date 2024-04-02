82016639bc9fa9b75609cac7002f6f54441c0008
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-02 17:31:59 +0200

    fix: added comments to snack 9

--- JSnack9/script.js
+++ JSnack9/script.js
@@ -1,12 +1,17 @@
 // Calcola la somma e la media dei primi 10 numeri.
 
+// creo una variabile per la somma
 let sum = 0
 
+// creo un ciclo per i primi 10 numeri
 for (let i = 0; i <= 10; i++){
+    // Incremento il numero precedente con in numero sucessivo in modo da ottenere la somma
     sum = sum + i;
 }
 
+// Creo la media dei 10 numeri
 const average = sum / 10
 
+// Stampo somma e media
 console.log(`La somma dei 10 numeri è: ${sum}`);
 console.log(`La media dei 10 numeri è: ${average}`);
